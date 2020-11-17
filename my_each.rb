54067407f83b47120e56233f37cce64de762dbb7
Nausheen Akhter <nausheen701@gmail.com>
2020-11-18 01:47:57 +0300

    commit tree 91791c8c556fdf82e20455c69ae1a97cb06cca96

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(colors)
+  if block_given?
+      i = 0
+     
+      while i < colors.length
+          yield(colors[i])
+          i = i + 1
+      end
+      
+      colors
+  else
+     "Hey! No block was given!"
+  end
 end
+
+["red", "blue", "green"]
